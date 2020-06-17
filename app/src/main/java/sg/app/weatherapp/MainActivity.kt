a9889c566332218e4855222a89923c47cea44211
jasotharan <jasotharan1990@gmail.com>
2020-06-17 11:37:08 +0800

    set day night theme

--- app/src/main/java/sg/app/weatherapp/MainActivity.kt
+++ app/src/main/java/sg/app/weatherapp/MainActivity.kt
@@ -11,11 +11,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import sg.app.weatherapp.databinding.ActivityMainBinding
 import sg.app.weatherapp.models.WeatherCatList
+import sg.app.weatherapp.util.StringUtil
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         progressDialog?.setMessage("Loading....")
         progressDialog?.show()
 
+        // search location
         binding.btnSearch.setOnClickListener {
             if (binding.etCityOrCountryName.text.isNotBlank()) {
                 viewModel.getWeatherDataBycity(binding.etCityOrCountryName.text.toString())
@@ -58,6 +61,7 @@ class MainActivity : AppCompatActivity() {
             et_city_or_country_name.visibility = View.VISIBLE
         }
 
+        // this for bottom recycle view
         viewModel.getFiveDaysList().observe(this, Observer<List<WeatherCatList>>
         { dataList ->
             progressDialog?.dismiss()
@@ -69,11 +73,21 @@ class MainActivity : AppCompatActivity() {
                 more_days_rv.adapter = WeatherAdapter(dataList, this,
                     { partItem: WeatherCatList ->
                         viewModel.updateDayTemperature(partItem)
+                        setBackgroundImg(partItem.dtTxt)
                     })
             } else {
                 Toast.makeText(this, "No result found", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun setBackgroundImg(dtTxt: String?) {
+        var isNight = dtTxt?.let { StringUtil().isNight(it) }!!
+        if (isNight) {
+            scroll_view.setBackgroundResource(R.drawable.bag_img_nt)
+        } else {
+            scroll_view.setBackgroundResource(R.drawable.bag_img)
+        }
     }
 
 }
--- app/src/main/java/sg/app/weatherapp/util/StringUtil.kt
+++ app/src/main/java/sg/app/weatherapp/util/StringUtil.kt
@@ -12,14 +12,33 @@ class StringUtil {
 
         return timeString
     }
+
     fun convertMilliToDay(date: String): String {
         val input_date = "2017-01-30 18:00:00"
-        val format1 = SimpleDateFormat("yyyy-mm-dd hh:mm:ss")
+        val format1 = SimpleDateFormat("yyyy-mm-dd HH:mm:ss")
         val dt1 = format1.parse(date)
         val format2: DateFormat = SimpleDateFormat("dd EEE hh a")
         val finalDay: String = format2.format(dt1)
 
         return finalDay
+    }
+
+    fun isNight(date: String): Boolean {
+        var night = true;
+        val format1 = SimpleDateFormat("yyyy-mm-dd HH:mm:ss")
+        val dt1 = format1.parse(date)
+        val format2: DateFormat = SimpleDateFormat("HH")
+        val hhDay: String = format2.format(dt1)
+
+
+        if (hhDay.toInt() >= 0 && hhDay.toInt() < 6) {
+            night = true
+        } else if (hhDay.toInt() <= 18) {
+            night = false
+        } else {
+            night = true
+        }
+        return night
     }
 
 
